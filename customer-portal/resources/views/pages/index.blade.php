44b977a8f4a2d5e09c7b9ab01bfceef9836f21a5
Lennart Bank <lennart@weroda.com>
2019-01-14 00:49:22 +0100

    [INIT] custom sass

--- customer-portal/resources/views/pages/index.blade.php
+++ customer-portal/resources/views/pages/index.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{$title}}</h1>
-<p>Dit is ons klantenportaal</p>
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="display-3">Welcome to weroda</h1>
+        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        <p>
+            <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
+            <a class="btn btn-secondary btn-lg" href="/register" role="button">Register</a>
+        </p>
+    </div>
+</div>
 @endsection
